283926d29fedacda9ca76c97ecf048480604abef
Jhonatan Nascimento <j.nascimento3138@gmail.com>
2024-01-18 02:14:55 -0300

    Finish Section 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
+import { EXAMPLES } from "./data.js";
 
 function App() {
+    const [selectedTopic, setSelectedTopic] = useState();
+
     function handleClick(selectedButon) {
-        console.log(selectedButon);
+        setSelectedTopic(selectedButon);
+    }
+
+    let tabContent = <p>Please select a topic</p>;
+
+    if (selectedTopic) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+            </div>
+        );
     }
 
     return (
@@ -15,6 +34,13 @@ function App() {
                 <section id="core-concepts">
                     <h2>Core Concepts</h2>
                     <ul>
+                        {CORE_CONCEPTS.map((conceptItem) => (
+                            <CoreConcept
+                                key={conceptItem.title}
+                                {...conceptItem}
+                            />
+                        ))}
+                        {/* 
                         <CoreConcept
                             title={CORE_CONCEPTS[0].title}
                             description={CORE_CONCEPTS[0].description}
@@ -22,26 +48,38 @@ function App() {
                         />
                         <CoreConcept {...CORE_CONCEPTS[1]} />
                         <CoreConcept {...CORE_CONCEPTS[2]} />
-                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} /> */}
                     </ul>
                 </section>
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton onClick={() => handleClick("components")}>
+                        <TabButton
+                            isSelected={selectedTopic === "components"}
+                            onClick={() => handleClick("components")}
+                        >
                             Components
                         </TabButton>
-                        <TabButton onClick={() => handleClick("jsx")}>
+                        <TabButton
+                            isSelected={selectedTopic === "jsx"}
+                            onClick={() => handleClick("jsx")}
+                        >
                             JSX
                         </TabButton>
-                        <TabButton onClick={() => handleClick("props")}>
+                        <TabButton
+                            isSelected={selectedTopic === "props"}
+                            onClick={() => handleClick("props")}
+                        >
                             Props
                         </TabButton>
-                        <TabButton onClick={() => handleClick("state")}>
+                        <TabButton
+                            isSelected={selectedTopic === "state"}
+                            onClick={() => handleClick("state")}
+                        >
                             State
                         </TabButton>
                     </menu>
-                    Dynamic content
+                    {tabContent}
                 </section>
             </main>
         </div>
