31fe33ef00d338d29d0b0b6ebb10b201e329c77c
Jhonatan Nascimento <j.nascimento3138@gmail.com>
2024-01-17 21:56:46 -0300

    Section 3: React Essentials - Components, JSX, Props, State and more

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
+
+function App() {
+    function handleClick(selectedButon) {
+        console.log(selectedButon);
+    }
+
+    return (
+        <div>
+            <Header />
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton onClick={() => handleClick("components")}>
+                            Components
+                        </TabButton>
+                        <TabButton onClick={() => handleClick("jsx")}>
+                            JSX
+                        </TabButton>
+                        <TabButton onClick={() => handleClick("props")}>
+                            Props
+                        </TabButton>
+                        <TabButton onClick={() => handleClick("state")}>
+                            State
+                        </TabButton>
+                    </menu>
+                    Dynamic content
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export default App;
