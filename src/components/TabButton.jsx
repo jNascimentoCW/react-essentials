b0480b46e759b518e49592df6c552183fd82851d
Jhonatan Nascimento <j.nascimento3138@gmail.com>
2024-01-18 22:04:28 -0300

    Finish the first React project

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,10 +1,12 @@
-export default function TabButton({ children, onClick, isSelected }) {
+// export default function TabButton({ children, onClick, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
     return (
         <li>
-            <button
+            {/* <button
                 className={isSelected ? "active" : "undefined"}
                 onClick={onClick}
-            >
+            > */}
+            <button className={isSelected ? "active" : "undefined"} {...props}>
                 {children}
             </button>
         </li>
---
+++ src/components/Tabs.jsx
@@ -0,0 +1,11 @@
+import { Children } from "react";
+
+export default function Tabs({ children, buttons, ButtonsContainer = "menu" }) {
+    // const ButtonsContainer = buttonsContainer;
+    return (
+        <>
+            <ButtonsContainer>{buttons}</ButtonsContainer>
+            {children}
+        </>
+    );
+}
